c473f11e565e42dc19bb03371a37dceee57ff57d
aadithpm <aadithpm@gmail.com>
2019-09-27 10:44:41 -0500

    Formatting, replacing assertEquals

--- tests/test_dynamic_array.py
+++ tests/test_dynamic_array.py
@@ -8,12 +8,12 @@ class DynamicArrayTest(unittest.TestCase):
         self.arr = DynamicArray(1)
 
     def test_capacity_on_create(self):
-        self.assertEquals(1, self.arr.capacity)
+        self.assertEqual(1, self.arr.capacity)
 
     def test_get_element(self):
         self.arr.add(5)
 
-        self.assertEquals(5, self.arr.get(0))
+        self.assertEqual(5, self.arr.get(0))
 
     def test_set_element(self):
         arr1 = DynamicArray(1)
@@ -24,14 +24,14 @@ class DynamicArrayTest(unittest.TestCase):
         arr2 = arr1
         arr2.set(0, 10)
 
-        self.assertEquals(arr2.get(0), 10)
-        self.assertNotEquals(arr2.get(0), arr1.get(1))
+        self.assertEqual(arr2.get(0), 10)
+        self.assertNotEqual(arr2.get(0), arr1.get(1))
 
     def test_size(self):
         self.arr.add(5)
         self.arr.add(6)
 
-        self.assertEquals(2, self.arr.size())
+        self.assertEqual(2, self.arr.size())
     
     def test_clear_array(self):
         self.arr.add(10)
@@ -39,13 +39,14 @@ class DynamicArrayTest(unittest.TestCase):
 
         self.arr.clear()
 
-        self.assertEquals(2, self.arr.size())
-        self.assertEquals(None, self.arr.get(0))
+        self.assertEqual(2, self.arr.size())
+        self.assertEqual(None, self.arr.get(0))
+
     def test_add_element(self):
         self.arr.add(1)
         self.arr.add(5)
 
-        self.assertEquals(str(self.arr), '[1,5]')
+        self.assertEqual(str(self.arr), '[1,5]')
 
     def test_add_element_increases_capacity_dynamically(self):
         start_capacity = self.arr.capacity
@@ -57,8 +58,8 @@ class DynamicArrayTest(unittest.TestCase):
 
     def test_remove_at(self):
         self.arr.add(5)
-        
-        self.assertEquals(5, self.arr.remove_at(0))
+
+        self.assertEqual(5, self.arr.remove_at(0))
         self.assertRaises(IndexError, lambda: self.arr.remove_at(0))
 
     def test_remove(self):
@@ -67,14 +68,14 @@ class DynamicArrayTest(unittest.TestCase):
         self.arr.add(100)
 
         self.arr.remove(100)
-        
-        self.assertEquals(-1, self.arr.index_of(100))
-        self.assertEquals(str(self.arr), '[50,60]')
+
+        self.assertEqual(-1, self.arr.index_of(100))
+        self.assertEqual(str(self.arr), '[50,60]')
 
     def test_index_of(self):
         self.arr.add(10)
 
-        self.assertEquals(0, self.arr.index_of(10))
+        self.assertEqual(0, self.arr.index_of(10))
     
     def test_contains(self):
         self.arr.add(5)
